29dd8b682e01bb26d681514333112386ae534adc
evgeny shlyk <serpupstars@gmail.com>
2023-07-12 17:56:00 +1000

    среда вечер

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,1 +1,25 @@
-new Swiper('slider-wrapper');
+const swiperContainer = document.querySelector('.swiper-container');
+const sliderInner = document.querySelector('.slider__inner');
+
+export const workSlider = () => {
+  if (swiperContainer) {
+    new Swiper('.slider-wrapper', {
+      navigation: {
+        nextEl: '.slider__button-next',
+        prevEl: '.slider__button-prev',
+      },
+      slidesPerView: 4,
+      spaceBetween: 40,
+      loop: true,
+    });
+  }
+  // if (sliderInner.style.width === '768px') {
+  //   Swiper('.slider-wrapper', {
+  //     slidesPerView: 2,
+  //     spaceBetween: 40,
+  //     loop: true,
+  //   });
+  // }
+};
+
+
