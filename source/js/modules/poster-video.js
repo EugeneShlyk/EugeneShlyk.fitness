5addbd1a91750e3b54bfdfb13007b5d041a9e857
EugeneShlyk <108038983+EugeneShlyk@users.noreply.github.com>
2023-07-31 13:23:39 +1000

    пнд 13-23

--- source/js/modules/poster-video.js
+++ source/js/modules/poster-video.js
@@ -3,6 +3,7 @@ const video = document.querySelector('.gym__video');
 export const changePoster = () => {
   console.log(window.innerWidth);
   if (window.innerWidth < '1200px' && window.innerWidth > '767px') {
-    video.poster = 'gym-video-tablet.jpg';
+    video.setAttribute('poster', 'img/gym-image/gym-video-tablet.jpg');
   }
 };
+// video.poster = 'gym-video-tablet.jpg';
